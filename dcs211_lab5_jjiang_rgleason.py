6dfe43b5543b73bdf6f8a00c42f026e9b21843f2
Ryan Gleason <ryangleason@bobcat-121-242.bates.edu>
2025-11-09 19:29:27 -0500

    made mistake

--- dcs211_lab5_jjiang_rgleason.py
+++ dcs211_lab5_jjiang_rgleason.py
@@ -107,7 +107,7 @@ def splitData(A: np.ndarray) -> List[np.ndarray]:
     y = A[:, -1]    # label column
 
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size = 0.2, random_state = 45
+        X, y, test_size = 0.2, random_state = 42
     )
 
     return [X_test, y_test, X_train, y_train]
