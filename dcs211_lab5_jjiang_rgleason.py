e5ceaad66a5abf1a67dcaf824f2a0135a8fdb220
Ryan Gleason <ryangleason@bobcat-121-242.bates.edu>
2025-11-09 21:09:34 -0500

    change

--- dcs211_lab5_jjiang_rgleason.py
+++ dcs211_lab5_jjiang_rgleason.py
@@ -304,7 +304,7 @@ def main():
     print("\n[INFO] Starting scikit-learn-assisted tests...")
 
     # Step 7: Split data
-    X_test, y_test, X_train, y_train = splitData(data_array)
+    X_test, y_test, X_train, y_train = splitData(A)
 
 
     # Step 8: Run guessed k-NN
